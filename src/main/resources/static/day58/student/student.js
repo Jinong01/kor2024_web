c66c0c9f6294ac50bc2eb8b61b3069a623149e04
jinong <sskawd@naver.com>
2025-01-09 19:04:35 +0900

    --

--- src/main/resources/static/day58/student/student.js
+++ src/main/resources/static/day58/student/student.js
@@ -31,3 +31,22 @@ function regist(){
     return;
 }
 
+function printAll(){
+    let studentHTML = document.querySelector('.studentlist')
+    fetch('/printall')
+    .then(r => r.json())
+    .then(d => {
+        let list = d
+        let HTML = 
+        '<h3>전체 학생들의 점수를 출력하는 페이지 입니다.</h3><br/><table><tr><th>학번</th><th>이름</th><th>국어점수</th><th>수학점수</th><th>영어점수</th></tr>'
+
+        for(let index =0; index <= list.length-1; index++){
+            student = list[index];
+            HTML += `<tr><th>${student.sno}</th><th>${student.sname}</th><th>${student.skor}</th><th>${student.smath}</th><th>${student.seng}</th>`
+        }   
+        
+        studentHTML.innerHTML =`${HTML}</table>`;
+    })
+    .catch(e => console.log(e))
+}
+
